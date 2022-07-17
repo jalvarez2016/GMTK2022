4e5397ecf05c1010e2e000e4266453159a6f395a
AGarciaNY <aldairgarcia360@gmail.com>
2022-07-16 20:04:27 -0400

    enemy damage

--- UI/TurnLabelScript.gd
+++ UI/TurnLabelScript.gd
@@ -4,6 +4,15 @@ onready var turnManager = load("res://UI/TurnManager.gd").new()
 onready var uiInfo = $UICanvas/UI/Info
 onready var buttons = $UICanvas/UI/Buttons
 
+#Math functions
+var rng = RandomNumberGenerator.new()
+func chooseEnemyMath(array):
+	rng.randomize()
+	var enemy = array[rng.randi_range(0, array.size() - 1)]
+	return enemy
+	
+#Gameplay logic
+
 func _ready():
 	# Add logic to assign turn to whoever is the fastest
 	match turnManager.turn:
@@ -15,13 +24,6 @@ func _ready():
 	turnManager.connect("ally_turn_started", self, "_on_EndTurn_ally_turn_started")
 	turnManager.connect("enemy_turn_started", self, "_on_EndTurn_enemy_turn_started")
 	
-func on_ally_turn_started():
-	pass
-	# Write the player turn logic here
-	# Show UI
-	# Add turnManager.turn = ENEMY_TURN to end of any attack/UI action
-	turnManager.turn = turnManager.ENEMY_TURN
-
 func _on_EndTurn_enemy_turn_started():
 	uiInfo.text = "It is the enemies turn"
 	print('enemy turn started')
@@ -33,15 +35,44 @@ func _on_EndTurn_enemy_turn_started():
 	# put the enemies turn on a timer
 	#turnManager.turn = turnManager.ALLY_TURN # Uncomment this when other enemy logic has been implemented
 	pass # Replace with function body.
-
+	
+#Quality of life function for ending player and enemy turns	
+	
+func end_player_turn():
+	for button in buttons:
+		button.disabled = true
+	turnManager.turn = turnManager.ENEMY_TURN
+	
+func end_enemy_turn():
+	for button in buttons:
+		button.disabled = false
+	turnManager.turn = turnManager.ALLY_TURN
+	
+	
+#Friend turn logic
 
 func _on_EndTurn_ally_turn_started():
 	for button in buttons:
 		button.disabled = false
 	print("ally turn started")
+	
+	
+# Button Logic
+
+func _on_Attack_pressed():
+	print('attacking')
+	var enemies = get_tree().get_nodes_in_group("Enemy");
+	print(enemies)
+	var enemy = chooseEnemyMath(enemies)
+	enemy.get_children()[2].value -= 10
+	#end_player_turn()
+	
+func _on_Ultimate_pressed():
+	print("ultimating")
+	
+	#end_player_turn()
 
 func _on_EndTurn_pressed():
 	print('endturn button pressed')
-	for button in buttons:
-		button.disabled = true
-	turnManager.turn = turnManager.ENEMY_TURN
+	end_player_turn()
+
